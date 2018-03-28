688875b2192748459fd1bd4d974665239250c8d4
Shinya Kumagai <roomful.rooms@gmail.com>
2018-03-28 23:56:00 +0900

    Add text list

--- app/src/main/java/com/github/droibit/stripedlist/sample/MainActivity.kt
+++ app/src/main/java/com/github/droibit/stripedlist/sample/MainActivity.kt
@@ -1,12 +1,67 @@
 package com.github.droibit.stripedlist.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.RecyclerView.VERTICAL
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.list
+
+typealias OnItemClickListener = (String) -> Unit
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    list.also {
+      it.adapter = TextListAdapter1(this, texts) {
+        Log.d(TAG, "#onItemClicked(text=$it)")
+      }
+      it.addItemDecoration(
+          DividerItemDecoration(this, VERTICAL)
+      )
+    }
+  }
+
+  companion object {
+
+    private val TAG = MainActivity::class.java.simpleName
+
+    // ref. https://randomwordgenerator.com/
+    private val texts = listOf(
+        "fail",
+        "trustee",
+        "innovation",
+        "respect",
+        "mainstream",
+        "glass",
+        "vnightmare",
+        "press",
+        "joint",
+        "mature",
+        "dome",
+        "rain",
+        "reservoir",
+        "organisation",
+        "door",
+        "collar",
+        "afford",
+        "excuse",
+        "reliable",
+        "frog",
+        "marketing",
+        "merit",
+        "borrow",
+        "carriage",
+        "improve",
+        "computing",
+        "vdistortion",
+        "vgasp",
+        "happen",
+        "foundation"
+    )
+
   }
 }
---
+++ app/src/main/java/com/github/droibit/stripedlist/sample/TextListAdapter1.kt
@@ -0,0 +1,45 @@
+package com.github.droibit.stripedlist.sample
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.droibit.stripedlist.sample.TextListAdapter1.ViewHolder
+import kotlinx.android.synthetic.main.list_item_text_1.view.text
+
+class TextListAdapter1(
+  context: Context,
+  private val texts: List<String>,
+  private val itemClickListener: OnItemClickListener
+) : RecyclerView.Adapter<ViewHolder>() {
+
+  private val inflater = LayoutInflater.from(context)
+
+  override fun onCreateViewHolder(
+    parent: ViewGroup,
+    viewType: Int
+  ) = ViewHolder(
+      itemView = inflater.inflate(R.layout.list_item_text_1, parent, false)
+  ).apply {
+    itemView.setOnClickListener {
+      itemClickListener.invoke(texts[adapterPosition])
+    }
+  }
+
+  override fun onBindViewHolder(
+    holder: ViewHolder,
+    position: Int
+  ) {
+    holder.bind(position, texts[position])
+  }
+
+  override fun getItemCount() = texts.size
+
+  class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(position: Int, text: String) {
+      itemView.text.text = text
+    }
+  }
+}
